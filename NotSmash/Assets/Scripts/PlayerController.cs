f90821b0103f3bf00e0bcb52bed1f73e28c21351
Zac Olsen <zolsen@masonlive.gmu.edu>
2016-07-10 19:06:09 -0400

    added horizontal movement

---
+++ NotSmash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5;
+
+	private Rigidbody2D rb2d;
+
+	void Start () {
+	
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+	
+		if (Input.GetAxis ("Horizontal") != 0) {
+			rb2d.velocity = new Vector2 (speed * Input.GetAxis ("Horizontal"), rb2d.velocity.y);
+		} else {
+			rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+		}
+	}
+}
