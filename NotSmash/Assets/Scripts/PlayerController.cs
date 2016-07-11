28ce433a2bde5c32a6e532f3b29e527911c974be
Swado95 <swado95@yahoo.com>
2016-07-10 20:05:37 -0400

    Vertical movement basic

--- NotSmash/Assets/Scripts/PlayerController.cs
+++ NotSmash/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5;
+    public float jumpF = 2;
+    public bool isGrounded;
 
 	private Rigidbody2D rb2d;
 
 	void Start () {
-	
+
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
@@ -16,8 +18,21 @@ public class PlayerController : MonoBehaviour {
 	
 		if (Input.GetAxis ("Horizontal") != 0) {
 			rb2d.velocity = new Vector2 (speed * Input.GetAxis ("Horizontal"), rb2d.velocity.y);
-		} else {
-			rb2d.velocity = new Vector2(0, rb2d.velocity.y);
 		}
-	}
+
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpF);
+                isGrounded = false;
+            }
+        }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "isGround")
+        {
+            isGrounded = true;
+        }
+    }
 }
+
