4111397feac32cc64b612b19dd63db9f3ae3758a
Zac Olsen <zolsen@masonlive.gmu.edu>
2016-07-10 20:23:54 -0400

    fixed jump

--- NotSmash/Assets/Scripts/PlayerController.cs
+++ NotSmash/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,27 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5;
-    public float jumpF = 2;
-    public bool isGrounded;
+    public float jumpF = 365;
 
 	private Rigidbody2D rb2d;
+	private bool isGrounded;
 
 	void Start () {
 
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
-	void Update () {
+	void FixedUpdate () {
 	
 		if (Input.GetAxis ("Horizontal") != 0) {
 			rb2d.velocity = new Vector2 (speed * Input.GetAxis ("Horizontal"), rb2d.velocity.y);
 		}
 
-        if (isGrounded == true && Input.GetButtonDown("Jump"))
-            {
-                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpF);
-                isGrounded = false;
-            }
+        if (isGrounded == true && Input.GetButtonDown("Jump")) {
+			rb2d.AddForce(new Vector2(0, jumpF));
+        	isGrounded = false;
         }
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -35,4 +34,3 @@ public class PlayerController : MonoBehaviour {
         }
     }
 }
-
