76417e61e2be08d2b89331f588a433e8bb49ff38
Zac Olsen <zolsen@masonlive.gmu.edu>
2016-07-16 22:38:04 -0400

    fixed cancer

--- NotSmash/Assets/Scripts/Enemy.cs
+++ NotSmash/Assets/Scripts/Enemy.cs
@@ -3,39 +3,26 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-    public float emy_health = 100;
-    public float emy_dmg = 5;
-    public bool emy_isAgro = false;
-    public float emy_dmgMuti = 1;
-    public float emy_speed = 4;
+    public int health = 100;
+	public int damage = 5;
+    public bool isAggro = false;
+	public int damageMulti = 1;
+	public int speed = 4;
     public GameObject target;
-
-
-
-	// Use this for initialization
-	void Start () {
-
-
-
-	}
-	
-	// Update is called once per frame
+		
 	void Update () {
 	
-       if(emy_isAgro == true)
+       if(isAggro == true)
         {
             transform.position = new Vector2(Vector2.MoveTowards(transform.position, 
-                target.transform.position, emy_speed * Time.deltaTime).x , transform.position.y);
-
+                target.transform.position, speed * Time.deltaTime).x , transform.position.y);
         }
-        
-
 	}
 
-    public void emyAttack(float __dmg, float __dmgMuti)
+    public void emyAttack(float dmg, float dmgMulti)
     {
-        __dmg = emy_dmg;
-        __dmgMuti = emy_dmgMuti;
+        damage = dmg;
+        damageMulti = dmgMulti;
         float rad = 1.0f;
 
         RaycastHit2D bCast = Physics2D.CircleCast(transform.position, rad, Vector2.right);
