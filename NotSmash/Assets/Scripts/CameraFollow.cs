1b8337bf85de171b1339decdccf5845326fd40ce
Swado95 <swado95@yahoo.com>
2016-07-17 21:09:21 -0400

    WorkingVersion

---
+++ NotSmash/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+	
+	void Update () {
+	
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+	}
+}
---
+++ NotSmash/Assets/Scripts/Enemy.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public int health = 100;
+	public int damage = 5;
+    public bool isAggro = false;
+	public float damageMulti = 1;
+	public int speed = 4;
+    public GameObject target;
+    public int jForce = 5;
+
+    private float tStamp = 0;
+    private int startHealth = 0;
+
+    void Start(){
+
+        startHealth = health;
+    }
+
+	void Update () {
+
+        
+
+        if (emyAggro() == true)
+        {
+            emyAiMovement(); 
+        }
+
+    }
+
+    public void emyAttack(int dmg, float dmgMulti)
+    {
+        damage = dmg;
+        damageMulti = dmgMulti;
+        float rad = 1.0f;
+
+        RaycastHit2D bCast = Physics2D.CircleCast(transform.position, rad, Vector2.right);
+        if(bCast.transform.tag == "Player")
+        {
+            //player health script   
+        }        
+    }
+
+    //__________________AI Movement_________________________
+    public void emyAiMovement()
+    {
+
+        float dis = Vector2.Distance(target.transform.position, transform.position);
+
+        transform.position = new Vector2(Vector2.MoveTowards(transform.position,
+            target.transform.position, speed * Time.deltaTime).x, transform.position.y);
+
+        //enemy only jumps when near target and target is higher then enemy
+        if (dis < 10 && (target.transform.position.y > transform.position.y + 5) && tStamp < Time.time)
+        {
+            transform.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jForce);
+            tStamp = Time.time + 3;
+        }
+    }
+
+    public bool emyAggro()
+    {
+        if (health != startHealth)
+        {
+            isAggro = true;
+        }
+
+        return isAggro;
+    }
+}
